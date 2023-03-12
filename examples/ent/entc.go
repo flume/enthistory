bf10bc0f48461aafe0e7e14e1fc4eaef94411b7b
Matthew Frisby <frisbm@gmail.com>
2023-03-12 15:51:13 -0500

    remove config

--- examples/ent/entc.go
+++ examples/ent/entc.go
@@ -8,12 +8,10 @@ import (
 	"github.com/frisbm/enthistory"
 
 	"entgo.io/ent/entc"
-	"entgo.io/ent/entc/gen"
 )
 
 func main() {
 	if err := entc.Generate("./schema",
-		&gen.Config{},
 		entc.Extensions(enthistory.NewHistoryExtension("userId")),
 	); err != nil {
 		log.Fatal("running ent codegen:", err)
