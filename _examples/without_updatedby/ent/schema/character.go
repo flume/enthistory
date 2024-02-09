fd7c564d059657a571e1cd132710f568758d5c6b
jan-4brainsolutions <159311009+jan-4brainsolutions@users.noreply.github.com>
2024-02-09 16:58:44 +0100

    fix: history schema fields now get the correct position index when updatedby is disabled (#36)

---
+++ _examples/without_updatedby/ent/schema/character.go
@@ -0,0 +1,56 @@
+package schema
+
+import (
+	"entgo.io/ent"
+	"entgo.io/ent/dialect/entsql"
+	"entgo.io/ent/schema"
+	"entgo.io/ent/schema/edge"
+	"entgo.io/ent/schema/field"
+
+	"github.com/flume/enthistory/_examples/basic/ent/schema/mixins"
+)
+
+// Character holds the schema definition for the Character entity.
+type Character struct {
+	ent.Schema
+}
+
+// Annotations of the Character.
+func (Character) Annotations() []schema.Annotation {
+	return []schema.Annotation{
+		entsql.Annotation{
+			Table: "character",
+		},
+	}
+}
+
+// Fields of the Character.
+func (Character) Fields() []ent.Field {
+	return []ent.Field{
+		field.Int("age").
+			Positive(),
+		field.String("name"),
+		field.JSON("nicknames", []string{}).
+			Optional(),
+		field.JSON("info", map[string]any{}).
+			Optional(),
+	}
+}
+
+// Edges of the Character.
+func (Character) Edges() []ent.Edge {
+	return []ent.Edge{
+		edge.To("friends", Character.Type).
+			Through("friendships", Friendship.Type),
+		edge.From("residence", Residence.Type).
+			Ref("occupants").
+			Unique(),
+	}
+}
+
+// Mixin of the Character.
+func (Character) Mixin() []ent.Mixin {
+	return []ent.Mixin{
+		mixins.TimeMixin{},
+	}
+}
