6caf5dd4abf07785bc68a3e2dc43c02b901a8ed6
Matthew Frisby <frisbm@gmail.com>
2024-03-03 11:00:56 -0600

    new debug project

---
+++ _examples/testdata/debug/entc.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"_examples/testdata/debug/schema"
+	"entgo.io/ent/entc"
+	"entgo.io/ent/entc/gen"
+	"fmt"
+	"log"
+
+	"entgo.io/ent"
+
+	"github.com/flume/enthistory"
+)
+
+const (
+	schemaPath = "./testdata/debug/schema"
+)
+
+func main() {
+	if err := enthistory.Generate(schemaPath, []ent.Interface{
+		&schema.Character{},
+		&schema.Friendship{},
+	},
+		enthistory.WithInheritIdType(),
+		enthistory.WithHistoryTimeIndex(),
+		enthistory.WithUpdatedBy("userid", enthistory.ValueTypeUUID),
+		enthistory.WithImmutableFields(),
+		enthistory.WithNillableFields(),
+	); err != nil {
+		log.Fatal(fmt.Sprintf("running enthistory codegen: %v", err))
+	}
+
+	if err := entc.Generate(schemaPath,
+		&gen.Config{
+			Target:  "./testdata/debug/internal/ent",
+			Schema:  "_examples/testdata/debug/schema",
+			Package: "_examples/testdata/debug/internal/ent",
+		},
+	); err != nil {
+		log.Fatal("running ent codegen:", err)
+	}
+}
---
+++ _examples/testdata/debug/schema/friendship.go
@@ -0,0 +1,47 @@
+package schema
+
+import (
+	"entgo.io/ent"
+	"entgo.io/ent/dialect/entsql"
+	"entgo.io/ent/schema"
+	"entgo.io/ent/schema/edge"
+	"entgo.io/ent/schema/field"
+	"github.com/google/uuid"
+)
+
+// Friendship holds the schema definition for the Friendship entity.
+type Friendship struct {
+	ent.Schema
+}
+
+// Annotations of the Friendship.
+func (Friendship) Annotations() []schema.Annotation {
+	return []schema.Annotation{
+		entsql.Annotation{
+			Table: "friendship",
+		},
+	}
+}
+
+// Fields of the Friendship.
+func (Friendship) Fields() []ent.Field {
+	return []ent.Field{
+		field.UUID("id", uuid.New()).Default(uuid.New),
+		field.UUID("character_id", uuid.New()),
+		field.UUID("friend_id", uuid.New()),
+	}
+}
+
+// Edges of the Friendship.
+func (Friendship) Edges() []ent.Edge {
+	return []ent.Edge{
+		edge.To("character", Character.Type).
+			Required().
+			Unique().
+			Field("character_id"),
+		edge.To("friend", Character.Type).
+			Required().
+			Unique().
+			Field("friend_id"),
+	}
+}
